bb064ad416389553bdb517db15bd26d4b174d7bb
marioaperezd <mario.pd@matamoros.tecnm.mx>
2024-08-15 18:53:53 -0500

    Add files via upload

---
+++ src/C_AND.v
@@ -0,0 +1,6 @@
+module C_AND (A,B,OUT);
+  input A,B;
+  output OUT;
+  
+  assign OUT=A & B;
+endmodule
---
+++ src/C_XOR.v
@@ -0,0 +1,6 @@
+module C_XOR (A,B,OUT);
+  input A,B;
+  output OUT;
+  
+  assign OUT=A ^ B;
+endmodule
